eb8f7681ec852701da3ad3f5dcc9dae5053cffa9
Daniel Hoang <hoang.daniel2005@gmail.com>
2025-12-08 18:05:29 +0000

    optimised day7 p2

--- day 7/p2.py
+++ day 7/p2.py
@@ -1,29 +1,13 @@
 with open('out', 'r') as f:
     grid = [list(i) for i in f.read().splitlines()]
 
-s = grid[0].index('S')
-
-# holy guess, conveniently setup for part 2
 rows, cols = len(grid), len(grid[0])
-visited = {}
-def dfs(r, c):
-    if (r, c) in visited:
-        return visited[(r, c)]
-    if r >= rows:
-        return 1
 
-    if grid[r][c] == '^':
-        res = 0
-        if c + 1 < cols:
-            res += dfs(r, c + 1)
-        if c - 1 >= 0:
-            res += dfs(r, c - 1)
+dp = [1] * cols
+for r in range(rows-2, -1, -1):
+    for c in range(cols):
+        if grid[r][c] == '^':
+            dp[c] = dp[c - 1] + dp[c + 1]
 
-        visited[(r, c)] = res
-        return res
-    else:
-        visited[(r, c)] = dfs(r + 1, c)
-        return visited[(r, c)]
-
-
-print(dfs(0, s))
+s = grid[0].index('S')
+print(dp[s])
